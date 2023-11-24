e5acda6d71fcc2b31cdc1b64998bcbc2c5a6ab20
Luis Ferreira <2211882@my.ipleiria.pt>
2023-11-24 01:48:39 +0000

    Dashboard Almost Done

--- DentalCare/backend/assets/AppAsset.php
+++ DentalCare/backend/assets/AppAsset.php
@@ -15,14 +15,17 @@ class AppAsset extends AssetBundle
         [
         'index/site.index',
         ],
-        'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css',
+            'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
 
     ];
     public $js = [
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
-        '\hail812\adminlte3\assets\AdminLteAsset'
+        '\hail812\adminlte3\assets\AdminLteAsset',
     ];
 }
