8a323d96b9f3b2b6750ccfa21c40c4a4604bb7e9
Kynotec <2211867@my.ipleiria.pt>
2024-01-06 03:23:56 +0000

    create categoria test

---
+++ DentalCare/backend/tests/unit/CategoriaTest.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace backend\tests\Unit;
+
+use backend\tests\UnitTester;
+use common\models\Categoria;
+use common\widgets\Alert;
+
+class CategoriaTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+        $categorias = new Categoria();
+    }
+
+
+
+    // tests
+
+
+    public function testCreateCategoriaUnsuccessfully()
+    {
+        $categorias = new Categoria();
+        $categorias->descricao = 'HNxM1eZMhUjeNCrLLwQYNRxPwxCfmjDb3E7dCtCRHfRzoLoOyQPL0vadgasdfsa';
+        $this->assertFalse($categorias->validate(['descricao']));
+        verify($categorias->save())->false();
+    }
+
+
+    public function testCreateCategoriaSuccessfully()
+    {
+        $categorias = new Categoria();
+        $categorias->descricao = 'pasta de dentes';
+        $this->assertTrue($categorias->validate(['descricao']));
+        verify($categorias->save())->true();
+        $this->assertNotNull(Categoria::findOne(['descricao' => $categorias->descricao]));
+        $this->tester->seeRecord('common\models\Categoria', array('descricao' => 'pasta de dentes'));
+    }
+
+    public function testCategoriaUpdate()
+    {
+        $categorias = $this->tester->grabRecord('common\models\Categoria', array('descricao' => 'pasta de dentes'));
+
+        $categorias = new Categoria();
+        $categorias->descricao = 'pasta de dentes';
+        $categorias->save();
+        $this->tester->dontSeeRecord('common\models\Categoria', array('descricao' => 'escova'));
+        $this->tester->seeRecord('common\models\Categoria', array('descricao' => 'pasta de dentes'));
+
+    }
+
+
+    public function testCategoriaDelete()
+    {
+        $categoria = new Categoria();
+        $categoria->delete();
+        $categorias = Categoria::findOne(['descricao' => 'escova']);
+        $this->assertNull($categorias);
+    }
+
+
+
+}
